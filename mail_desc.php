eef9168f02080a0c42320c36c46d582193d81393
Dima Urazov <itm21.top@gmail.com>
2017-12-16 15:30:02 +0200

    Третий комит

---
+++ mail_desc.php
@@ -0,0 +1,50 @@
+<?php
+ header('Content-Type: text/html;charset=utf-8');
+
+
+
+
+  $f1q0 = $_POST['name'];
+  $f1q1 = $_POST['phone'];
+  $f1q2 = $_POST['coments'];
+
+  
+  $to = "Sergey Glotov <attorney@sglotov.com>".", ";
+  $to = "Developer <itm21.top@gmail.com>";
+ 
+  
+
+  $subject = "Заявка с сайта";
+  $headers = "From: Сайт Sergey Glotov <attorney@sglotov.com>\r\n";
+  $headers .= "Content-type: text/plain; Charset=UTF-8\r\n";
+
+  if($f1q2!='')
+  {
+    $message = "Пользователь заполнил Заявку c компьютера:
+
+
+    Имя - $f1q0
+
+    Телефон - $f1q1
+
+    Сообщение - $f1q2
+
+    ";
+  }
+  else
+  {
+    $message = "Пользователь заполнил Заявку c телефона:
+
+
+    Имя - $f1q0
+
+    Телефон - $f1q1
+
+    ";
+  }
+
+  
+  mail($to, $subject, $message, $headers);
+  
+
+?>
